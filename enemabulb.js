36b1173765b7d8d6cba7eb7f71a4d0573b6e25f5
Kei Tawara <tawak3500@gmail.com>
2018-09-24 17:34:29 -0400

    no message

---
+++ enemabulb.js
@@ -0,0 +1,44 @@
+var product = "enema"; //choose which product
+        var position = "1_1";
+        var position2 = "8_1";
+        var keyword1;  //high priority keyword
+        var keyword2; // low priority keyword
+        var keyword3; // low priority keyword
+        var keywordtemp;
+        var asin;
+        var canonical;
+        var ts = Math.round(new Date().getTime() / 1000);//get the unix timestap
+        var superurl;
+        var randomInt =  parseInt(Math.floor(Math.random() * 4) + 1); //create random number between 1-4
+
+//window.alert(randomInt); //show windows box popup
+
+//====================================================================================
+//====================================================================================
+//====================================================================================
+if(product == "enema")
+    {
+        keyword1 = "enema+bulb";
+        keyword2 = "enema+bulb";
+        asin = "B07FV7D4CS";
+        canonical = "Large-Enema-Bulb-Kit-Phthalates";
+  //====================================================================================      
+        if(randomInt == 1 || randomInt == 2)
+            {
+                keywordtemp = keyword1;
+            }
+        else if (randomInt == 3)
+            {
+                keywordtemp = keyword2;
+            }
+        //==========================
+        if(keywordtemp != null)
+            {
+                 superurl = ("https://www.amazon.com/"+ canonical + "/dp/"+ asin + "/ref=sr_" + position + "_a_it?ie=UTF8&qid=" + ts + "&sr=" + position2 + "&keywords=" + keywordtemp);
+            window.location.href = superurl; 
+            }   
+    }
+
+//====================================================================================
+//====================================================================================
+//====================================================================================
